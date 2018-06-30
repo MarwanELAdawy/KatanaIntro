23b1dba18c3eb351853c5407bab2f0751012381a
MarwanELAdawy <marwan.mahmoud@cis.asu.edu.eg>
2018-06-30 19:21:55 +0200

    A simple appliation that run the server from OWIN library

---
+++ KatanaIntro/Program.cs
@@ -0,0 +1,36 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace KatanaIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "http://localhost:8080";
+
+            using (WebApp.Start<startup>(uri))
+            {
+                Console.WriteLine("Started!");
+                Console.ReadKey();
+                Console.WriteLine("Stooping");
+            }
+        }
+    }
+        public class startup
+        {
+            public void Configuration(IAppBuilder app)
+            {
+                app.Run (ctx => 
+                {
+                    return ctx.Response.WriteAsync("Hello World");
+                });
+            }
+        }
+    }
+
