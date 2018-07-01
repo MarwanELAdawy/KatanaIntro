a2635111c5c2833b30c3f907e281ab6ffcfdd011
MarwanELAdawy <marwan.mahmoud@cis.asu.edu.eg>
2018-07-01 09:12:42 +0200

    IIS Update

--- KatanaIntro/Program.cs
+++ KatanaIntro/Program.cs
@@ -6,27 +6,51 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Owin.Hosting;
 using System.IO;
+using System.Web.Http;
 
 namespace KatanaIntro
 {
     using AppFunc = Func<IDictionary<string, object>, Task>;
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string uri = "http://localhost:8080";
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        string uri = "http://localhost:8080";
 
-            using (WebApp.Start<startup>(uri))
-            {
-                Console.WriteLine("Started!");
-                Console.ReadKey();
-                Console.WriteLine("Stooping");
-            }
-        }
-    }
+    //        using (WebApp.Start<startup>(uri))
+    //        {
+    //            Console.WriteLine("Started!");
+    //            Console.ReadKey();
+    //            Console.WriteLine("Stooping");
+    //        }
+    //    }
+    //}
     public class startup
     {
         public void Configuration(IAppBuilder app)
+        {
+            app.Use(async (environment, next) =>
+            {
+                Console.WriteLine("Requesting : " + environment.Request.Path);
+                await next();
+                Console.WriteLine("Response: " + environment.Response.StatusCode);
+            });
+            ConfigurationWebApi(app);
+            app.UseHelloWorld();
+        }
+        public void ConfigurationWebApi(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional});
+            app.UseWebApi(config);
+        }
+    }
+    public static class AppBuilderExtention
+    {
+        public static void UseHelloWorld(this IAppBuilder app)
         {
             app.Use<HelloWorldComponent>();
         }
