84f2a15316d7c1144989b2935921131bca72d4da
MarwanELAdawy <marwan.mahmoud@cis.asu.edu.eg>
2018-07-01 08:09:35 +0200

    Update program.cs

--- KatanaIntro/Program.cs
+++ KatanaIntro/Program.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Owin.Hosting;
+using System.IO;
 
 namespace KatanaIntro
 {
+    using AppFunc = Func<IDictionary<string, object>, Task>;
     class Program
     {
         static void Main(string[] args)
@@ -22,16 +24,29 @@ namespace KatanaIntro
             }
         }
     }
-        public class startup
+    public class startup
+    {
+        public void Configuration(IAppBuilder app)
         {
-            public void Configuration(IAppBuilder app)
-            {
-            app.UseWelcomePage();
-                //app.Run (ctx => 
-                //{
-                //    return ctx.Response.WriteAsync("Hello World");
-                //});
-            }
+            app.Use<HelloWorldComponent>();
         }
     }
+    public class HelloWorldComponent
+    {
+        AppFunc _next;
+        public HelloWorldComponent(AppFunc next)
+        {
+            _next = next;
+        }
+        public Task Invoke (IDictionary<string,object> envirnoment)
+        {
+            var response = envirnoment["Owin.ResponseBody"] as Stream;
+            using (var writer = new StreamWriter(response))
+            {
+                return writer.WriteAsync("Hello !!!");
+            }
+        }
+
+    }
+}
 
